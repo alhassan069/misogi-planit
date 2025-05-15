6253c31a229368511f595b4a1cd90fac3106cb27
Alimul Hassan <alhassan069@gmail.com>
2025-05-15 18:30:48 +0530

    added sqlite compatibility

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,7 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config();
-const process = require('process');
-
 
 const database = process.env.DB_NAME;
 const username = process.env.DB_USER;
@@ -10,16 +9,22 @@ const password = process.env.DB_PASS;
 const host = process.env.DB_HOST;
 const dialect = process.env.DB_DIALECT;
 const port = process.env.DB_PORT;
-
-
-// const db_url = `mysql://${username}:${password}@${host}:${port}/${database}`;
-
-// const sequelize = new Sequelize(db_url);
-const sequelize = new Sequelize(database, username, password, {
-  host: host,
-  dialect: dialect,
-  port: port,
-});
+let sequelize;
+if (dialect === 'sqlite') {
+  // SQLite configuration
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, '../database.sqlite'), // Store the database file in the server directory
+    logging: console.log, // Set to false to disable SQL query logging
+  });
+} else {
+  // MySQL configuration
+  sequelize = new Sequelize(database, username, password, {
+    host: host,
+    dialect: dialect,
+    port: port,
+  });
+}
 
 module.exports = sequelize;
 
