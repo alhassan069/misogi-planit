ff2c3a8aab2fdede21f7ba3a1c96f0648f983963
Alimul Hassan <alhassan069@gmail.com>
2025-05-15 19:38:32 +0530

    checking pathnames in server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
+const path = require('path');
+console.log(__dirname);
+console.log(path.join(__dirname, '..'));
+console.log(path.join(__dirname, '..', '..'));
+console.log(path.join(__dirname, '..', '..', '..'));
+console.log(path.join(__dirname, '..', '..', '..', '..'));
+console.log(path.join(__dirname, '..', '..', '..', '..', '..'));
+console.log(path.join(__dirname, '..', '..', '..', '..', '..', '..'));
+console.log(path.join(__dirname, '..', '..', '..', '..', '..', '..', '..'));
+console.log(path.join(__dirname, '..', '..', '..', '..', '..', '..', '..', '..'));
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
